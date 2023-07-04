ef057c83cdb8246d40029fb898748bacc5811df4
Harik35 <138478159+Harik35@users.noreply.github.com>
2023-07-04 14:35:06 +0530

    Add files via upload

---
+++ Program_updated.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter the Word");
+string word = Console.ReadLine();
+Console.WriteLine("Enter the number");
+int.TryParse(Console.ReadLine(), out int num);
+Console.WriteLine("Enter the Charectetr");
+char.TryParse(Console.ReadLine(), out char letter);
+Console.WriteLine("Enter the boolean");
+bool.TryParse(Console.ReadLine(),out bool valu);
+
+
+Console.WriteLine(word);
+Console.WriteLine(num);
+Console.WriteLine(letter);
+Console.WriteLine(valu);
+
+string write = Console.ReadLine();
