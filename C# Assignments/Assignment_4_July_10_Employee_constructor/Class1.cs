0ea52bb142ac3ed6824f4d8e6c2fc8d9de5d5432
Haril35 <haripm.krishnan@gmail.com>
2023-07-11 00:53:11 +0530

    Assignmet

---
+++ C# Assignments/Assignment_4_July_10_Employee_constructor/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee1
+{
+    public class Employee
+    {
+        public int Id;
+        public string Name;
+        public double Salary;
+
+        public Employee(int id)
+        {
+            Id = id;
+        }
+
+        public Employee(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Employee Details:");
+            Console.WriteLine("ID: " + Id);
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Salary: " + Salary);
+        }
+    }
+
+}
