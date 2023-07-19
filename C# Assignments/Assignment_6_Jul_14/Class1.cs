dac2f3fc878fe7b0c2dfcb16918d6960b122b386
Harik35 <harikrishnan.pm@controlqore.com>
2023-07-19 09:49:27 +0530

    C#Assignment_6

---
+++ C# Assignments/Assignment_6_Jul_14/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfJoin { get; set; }
+        public string Department { get; set; }
+        public double Salary { get; set; }
+        public char Jobtype { get; set; }
+        public Employee(int id, string name, DateTime datetime, string dept, double salary, char jobtype)
+        {
+            Id = id;
+            Name = name;
+            DateOfJoin = datetime;
+            Department = dept;
+            Salary = salary;
+            Jobtype = jobtype;
+        }
+    }
+    interface IBonusCalculator
+    {
+        double CalculateBonus(Employee employee1);
+    }
+    class PermanentEmployeeBonusCalculator : IBonusCalculator
+    {
+        public double CalculateBonus(Employee employee1)
+        {
+            return 0.2 * employee1.Salary;
+        }
+    }
+    class ContractEmployeeBonusCalculator : IBonusCalculator
+    {
+        public double CalculateBonus(Employee employee1)
+        {
+            return 0.15 * employee1.Salary;
+        }
+    }
+}
---
+++ C# Assignments/Assignment_6_Jul_14/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp9;
+
+Employee employee1 = new Employee(1, "Hari", DateTime.Now, "Dev", 15000, 'P');
+IBonusCalculator ibonus = null;
+if (employee1.Jobtype == 'P')
+{
+    ibonus = new PermanentEmployeeBonusCalculator();
+}
+else if (employee1.Jobtype == 'C')
+{
+    ibonus = new ContractEmployeeBonusCalculator();
+}
+
+double bonusAmt = ibonus.CalculateBonus(employee1);
+Console.WriteLine("Bonus amount:" + bonusAmt);
