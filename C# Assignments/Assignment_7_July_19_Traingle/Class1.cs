659e56d9e7aab6c64b3811b92e1b7e5017f6dbcd
Harik35 <harikrishnan.pm@controlqore.com>
2023-07-19 19:16:12 +0530

    Assignment_7_C#_Triangle

---
+++ C# Assignments/Assignment_7_July_19_Traingle/Class1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace July_19_Traingle
+{
+    public abstract class Shape
+    {
+        public Shape(int sides)
+        {
+            NoOfSides = sides;
+        }
+        public int NoOfSides { get; set; }
+        public abstract void DisplayNoOfSides();
+        public abstract double CalculateArea();
+    }
+
+    class Triangle: Shape
+    {
+        public Triangle(int sides, int side1, int side2, int side3 ) : base(sides)
+        {
+            NoOfSides = sides;
+            SideLength1 = side1;
+            SideLength2 = side2;
+            SideLength3 = side3;
+        }
+        public int SideLength1 { get; set; }
+        public int SideLength2 { get; set; }
+        public int SideLength3 { get; set; }
+
+        public override double CalculateArea()
+        {
+            double S = (SideLength1 + SideLength2 + SideLength3) / 2;
+            return S * (S - SideLength1) * (S - SideLength2) * (S - SideLength3);
+        }
+
+        public override void DisplayNoOfSides()
+        {
+            Console.WriteLine("Halleo");
+        }
+    }
+
+    class Square : Shape
+    {
+        public Square(int sides, int side) : base(side)
+        {
+            NoOfSides = sides;
+            Length = side;
+            
+        }
+        public int Length { get; set; }
+       
+
+        public override double CalculateArea()
+        {
+            return Length* Length;
+        }
+
+        public override void DisplayNoOfSides()
+        {
+            Console.WriteLine("Halleo");
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        private static int side;
+
+        public Rectangle(int sides, int side1, int side2) : base(side)
+        {
+            NoOfSides = sides;
+            Length = side1;
+            Width = side2;
+
+        }
+        public int Length { get; set; }
+        public int Width { get; set; }
+
+        public override double CalculateArea()
+        {
+            return Length * Width;
+        }
+
+        public override void DisplayNoOfSides()
+        {
+            Console.WriteLine("Halleo");
+        }
+    }
+
+}
---
+++ C# Assignments/Assignment_7_July_19_Traingle/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using July_19_Traingle;
+
+Shape obj1 = new Triangle(3, 10, 12, 15);
+Shape obj2 = new Rectangle(2, 10, 5);
+Shape obj3 = new Square(2, 10);
+double area1 = obj1.CalculateArea();
+Console.WriteLine($"Area of Triangle is {area1} ");
+double area2 = obj2.CalculateArea();
+Console.WriteLine($"Area of Triangle is {area2} ");
+double area3 = obj3.CalculateArea();
+Console.WriteLine($"Area of Triangle is {area3} ");
