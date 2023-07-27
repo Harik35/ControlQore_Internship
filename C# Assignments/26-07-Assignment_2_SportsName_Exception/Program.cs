9705445d4160afbdac183e087918724a3310762b
Harik35 <harikrishnan.pm@controlqore.com>
2023-07-27 15:07:06 +0530

    26-07-Assignment_2_SportsName_Exception

---
+++ C# Assignments/26-07-Assignment_2_SportsName_Exception/Program.cs
@@ -0,0 +1,33 @@
+﻿using SportsName;
+
+
+
+Console.WriteLine("Enter the code for sports Name:");
+char.TryParse(Console.ReadLine(), out char customName);
+
+
+try
+{
+    switch (customName)
+    {
+        case 'c':
+            Console.WriteLine("Cricket");
+            break;
+        case 'f':
+            Console.WriteLine("Football");
+            break;
+        case 'h':
+            Console.WriteLine("Hockey");
+            break;
+        case 't':
+            Console.WriteLine("Tennis");
+            break;
+        case 'b':
+            Console.WriteLine("Badminton");
+            break;
+        default:
+            throw new CustomException("Invalid entry, please enter any of the following option (c,f,h,t,b)");
+    }
+}
+catch(CustomException msg)
+{ Console.WriteLine(msg.ToString()); }
