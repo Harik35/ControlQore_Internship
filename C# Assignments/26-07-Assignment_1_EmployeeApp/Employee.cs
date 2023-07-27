a7116f50ea076088bb4eefd0d9d7fddf053a87ae
Harik35 <harikrishnan.pm@controlqore.com>
2023-07-27 14:11:15 +0530

    26-07-Assignment_1_EmployeeApp

---
+++ C# Assignments/26-07-Assignment_1_EmployeeApp/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.App
+{
+    public class Employee
+    {
+        public static int empNo = 1000;
+        public string empID { get; private set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string EmployeeType { get; set; }
+
+        public  Employee(string name, double salary, string empType) 
+        {
+            empID = "Emp" + empNo;
+            Name = name;
+            Salary = salary;
+            EmployeeType = empType;
+            empNo++;
+        }
+
+
+        public void Display()
+        {
+            Console.WriteLine(empID);
+        }
+        public static void DisplayNo() 
+        {
+            Console.WriteLine($"Total Number of Employees:{empNo-1000} ");
+        }
+    }
+}
---
+++ C# Assignments/26-07-Assignment_1_EmployeeApp/Program.cs
@@ -0,0 +1,13 @@
+﻿Employee.App.Employee emp1 = new("John Doe", 15000, "Permenant");
+Employee.App.Employee emp2 = new("Liam Smith", 20000, "Contract");
+Employee.App.Employee emp3 = new("Mary James", 15000, "Contract");
+
+
+emp1.Display();
+emp2.Display();
+emp3.Display();
+
+Employee.App.Employee.DisplayNo();
+
+
+
