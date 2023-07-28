1325bcff5092e3db64e4b46682763e9dde9574d3
Harik35 <harikrishnan.pm@controlqore.com>
2023-07-28 10:46:46 +0530

    26-07-Assignment_3_Extention_Method

---
+++ C# Assignments/26-07-Assignment_3_Extention_Method/CustomExtention.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    public static class StringExtensions
+    {
+        public static int isWordCount(this string text, string word)
+        {
+            int count = 0;
+            string[] words = text.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i] == word)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
+}
---
+++ C# Assignments/26-07-Assignment_3_Extention_Method/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExtensionMethod;
+
+
+//string word1 = "through an arrayn an  an of int";
+//string word2 = "an";
+
+//int num = word1.isWordCount(word2);
+
+//Console.WriteLine($"Number of occurence of the word '{word2}' is {num}");
+
+public class testingclass
+{
+    public int obj { get; set; }
+    public static subject
+
+    
+    public testingclass(int subject) 
+    {
+
+    }
+}
+
