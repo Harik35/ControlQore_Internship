699684df4010485e8fd0419f8bbf7d37c1b23221
Harik35 <harikrishnan.pm@controlqore.com>
2023-07-28 11:06:20 +0530

    LoopThroughArray

---
+++ C# Assignments/26-07-Assignment_4_LoopthroughArray/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+List<int> numArray2 = new List<int>();
+
+
+int[] numArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+
+foreach (int num in numArray)
+{
+    numArray2.Add(num * 2);
+}
+
+foreach (int num in numArray2)
+{
+    Console.WriteLine(num);
+}
