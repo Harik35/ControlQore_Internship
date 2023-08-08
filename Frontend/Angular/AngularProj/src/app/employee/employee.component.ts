f798efeeab0c0902513b30e482a05ce1284a74a5
Harik35 <harikrishnan.pm@controlqore.com>
2023-08-08 18:53:14 +0530

    employee

---
+++ Frontend/Angular/AngularProj/src/app/employee/employee.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+interface IemployeeDetails {
+  FirstName: string;
+  LastName: string;
+  DateofBirth: string;
+  Age: number;
+  JoiningDate: string;
+  Country: string;
+}
+
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.sass']
+})
+export class EmployeeComponent {
+public empdetails:Array<IemployeeDetails> = [
+  {
+    FirstName: "Harikrishnan",
+    LastName: "PM",
+    DateofBirth: '01/04/2000',
+    Age: 23,
+    JoiningDate: '03/06/2023',
+    Country: "India"
+  },{
+    FirstName: "Jubiya",
+    LastName: "Joseph",
+    DateofBirth: '03/05/2000',
+    Age: 23,
+    JoiningDate: '03/06/2023',
+    Country: "India"
+  },{
+    FirstName: "Ruby",
+    LastName: "Susan",
+    DateofBirth: '03/05/2000',
+    Age: 23,
+    JoiningDate: '03/06/2023',
+    Country: "India"
+  },
+  
+]
+
+}
---
+++ Frontend/Angular/AngularProj/src/app/employee/iemployee-details.ts
@@ -0,0 +1,8 @@
+export interface IemployeeDetails {
+    FirstName: string;
+    LastName: string;
+    DateofBirth: string;
+    Age: number;
+    JoiningDate: string;
+    Country: string;
+}
