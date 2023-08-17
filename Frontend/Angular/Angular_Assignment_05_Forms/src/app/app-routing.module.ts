6abd1250731aa3ed4e9a8409c4d376752021c294
Harik35 <harikrishnan.pm@controlqore.com>
2023-08-17 18:57:31 +0530

    Template Form

--- Frontend/Angular/Angular_Assignment_05_Forms/src/app/app-routing.module.ts
+++ Frontend/Angular/Angular_Assignment_05_Forms/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+
 import { EmployeeComponent } from './EmployeeDetails/employee/employee.component';
 import { DetailsComponent } from './EmployeeDetails/details/details.component';
-
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'
+ 
 const routes: Routes = [{
   path: 'employee', component: EmployeeComponent
 },{
