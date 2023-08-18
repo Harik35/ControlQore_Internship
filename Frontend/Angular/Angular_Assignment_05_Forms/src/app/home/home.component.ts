704169add0c6a6ede7547a136c748d72c88d15fb
Haril35 <haripm.krishnan@gmail.com>
2023-08-18 08:21:34 +0530

    Form

--- Frontend/Angular/Angular_Assignment_05_Forms/src/app/home/home.component.ts
+++ Frontend/Angular/Angular_Assignment_05_Forms/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
